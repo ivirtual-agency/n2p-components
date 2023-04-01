f98dcb856d3ef3b8ff5149b1abb033852f34fe0c
Francisco Kraefft <fkraefft@ivirtual.la>
2023-04-01 12:55:30 -0300

    Text & links working in menu list item of footer.

--- resources/views/components/ui/footer/menu-list-item.blade.php
+++ resources/views/components/ui/footer/menu-list-item.blade.php
@@ -1,11 +1,14 @@
 @props(['href', 'route'])
 
 <li>
-    <a {{ $attributes
-        ->merge(['class' => 'text-base text-gray-300 hover:text-white'])
-        ->except(['href', 'route'])
-        ->merge(['href' => $href ?? route($route)]) 
-    }}>
-        {{ $slot }}
-    </a>
+    @if (isset($href) || isset($route))
+        <a {{ $attributes
+            ->merge(['class' => 'text-base text-gray-300 hover:text-white'])
+            ->except(['href', 'route'])
+            ->merge(['href' => $href ?? route($route)]) 
+        }}>{{ $slot }}</a>
+    @else
+        <p {{ $attributes->merge(['class' => 'text-base text-gray-300'])
+        }}>{{ $slot }}</p>
+    @endif
 </li>
