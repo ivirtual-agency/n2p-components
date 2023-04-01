37a4ba4451d34eb4c7048bc7ecbfef0ffa73a743
Francisco Kraefft <fkraefft@ivirtual.la>
2023-04-01 15:18:07 -0300

    Stats section component added.

---
+++ resources/views/components/sections/stats.blade.php
@@ -0,0 +1,31 @@
+@props(['items' => [], 'icon' => 'entypo-plus'])
+
+<section {{ $attributes->merge(['class' => 'relative overflow-hidden pb-32']) }}>
+
+    {{ $slot }}
+
+    <div class="mx-auto max-w-7xl pt-10 flex flex-wrap text-center sm:text-left">
+
+        <h2 class="w-full sm:w-2/5 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl mb-12">
+            {{ $title }}
+        </h2>
+
+        <div class="w-full sm:w-3/5 flex space-x-0 sm:space-x-4">
+            @foreach ($items as $item)
+                <div class="flex-1 text-secondary">
+                    <div class="flex justify-center sm:justify-start mt-4 sm:mt-0">
+                        <span class="text-6xl">
+                            {{ $item['number'] }}
+                        </span>
+                        <span>
+                            @svg($icon, 'h-6 w-6')
+                        </span>
+                    </div>
+                    <p class="mt-3 text-base sm:mt-5 sm:text-xl">
+                        {{ $item['text'] }}
+                    </p>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</section>
