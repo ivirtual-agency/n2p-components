82e5347960a07e782f033742701ddf1c845ce604
Francisco Kraefft <fkraefft@ivirtual.la>
2023-04-01 10:38:58 -0300

    Master layout change of folder. Components working as anonymous.

---
+++ src/Net2phoneServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace iVirtual\Net2phone;
+
+use Illuminate\Support\Facades\Blade;
+use Spatie\LaravelPackageTools\Commands\InstallCommand;
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class Net2phoneServiceProvider extends PackageServiceProvider
+{
+    /**
+     * Configure the net2phone service provider.
+     * 
+     * @link https://github.com/spatie/laravel-package-tools
+     */
+    public function configurePackage(Package $package): void
+    {
+        $package
+            ->name('net2phone')
+            ->hasConfigFile()
+            ->hasViews()
+            ->hasAssets()
+            ->hasInstallCommand(function (InstallCommand $command) {
+                $command->publishAssets();
+            });
+    }
+
+    /**
+     * The package has been booted.
+     */
+    public function packageBooted(): void
+    {
+        Blade::anonymousComponentPath(
+            __DIR__ . '/../resources/views/components',
+            'n2p'
+        );
+    }
+}
