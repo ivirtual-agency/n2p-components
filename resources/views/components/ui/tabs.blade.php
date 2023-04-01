ce29a27ab9e33cc6ab0a8187b08e61f67f8c4460
Francisco Kraefft <fkraefft@ivirtual.la>
2023-04-01 15:50:59 -0300

    Tabs component working.

---
+++ resources/views/components/ui/tabs.blade.php
@@ -0,0 +1,65 @@
+@props([
+    'items' => 0,
+    'dark' => false
+])
+
+<div class="mx-auto mt-8 max-w-7xl sm:flex sm:flex-wrap sm:justify-center md:mt-12" x-data="{ selected: 0 }">
+    <div class="hidden sm:flex mt-8 w-full justify-center">
+        @for ($i = 0; $i < $items; $i++)
+            <button type="button" class="pb-4 px-4 border-b-2 text-lg" @click="selected = {{ $i }}" :class="{ 
+                    '{{ $dark ? 'text-white' : 'text-black' }} border-primary': selected === {{ $i }}, 
+                    '{{ $dark ? 'border-gray-400 text-gray-400' : 'border-gray-100 text-gray-500' }}': selected !== {{ $i }}
+                }">
+                <span>{{ ${'item_' . $i}->attributes->get('title') }}</span>
+            </button>
+        @endfor
+    </div>
+
+    <div class="flex flex-wrap mt-8 space-y-4">
+
+        @for ($i = 0; $i < $items; $i++)
+
+            {{-- Mobile Button --}}
+            <div class="w-full block sm:hidden">
+                <button type="button" @click="selected = (selected === {{ $i }} ? null : {{ $i }})"
+                    class="w-full flex justify-between items-center px-4 py-2 bg-gray-100">
+                    <p>{{ ${'item_' . $i}->attributes->get('title') }}</p>
+                    <span x-show="selected !== {{ $i }}">
+                        @svg('entypo-plus', 'w-8 h-8')
+                    </span>
+                    <span x-show="selected === {{ $i }}">
+                        @svg('entypo-minus', 'w-8 h-8')
+                    </span>
+                </button>
+            </div>
+            {{-- End Mobile Button --}}
+
+            {{-- Content --}}
+            <div x-show="selected === {{ $i }}" class="flex flex-wrap" 
+                x-transition:enter="transition ease-out duration-1000"
+                x-transition:enter-start="opacity-0 translate-y-1" 
+                x-transition:enter-end="opacity-100 translate-y-0">
+
+                @if (${'item_' . $i}->attributes->get('image'))
+                    {{-- With Image --}}
+                    <div class="w-full sm:w-2/5 sm:rounded-lg p-8 flex justify-center items-center">
+                        <img src="{{ asset(${'item_' . $i}->attributes->get('image')) }}" 
+                            alt="{{ ${'item_' . $i}->attributes->get('title') }}"
+                            title="{{ ${'item_' . $i}->attributes->get('title') }}"
+                            loading="lazy">
+                    </div>
+        
+                    <div class="w-full sm:w-3/5 text-left flex flex-col justify-center p-8">
+                        {{ ${'item_' . $i} }}
+                    </div>
+                @else
+                    {{-- Fully custom --}}
+                    <div class="w-full p-8">
+                        {{ ${'item_' . $i} }}
+                    </div>
+                @endif
+            </div>
+            {{-- End Content --}}
+        @endfor
+    </div>
+</div>
