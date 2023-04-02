d29844e0523c56935e94e22e7512cf5d560d3478
Francisco Kraefft <fkraefft@ivirtual.la>
2023-04-02 18:50:37 -0300

    Feature section component added.

---
+++ resources/views/components/sections/features.blade.php
@@ -0,0 +1,18 @@
+<section class="bg-secondary">
+
+    @svg('n2p-secondary-top-divider', 'w-full')
+
+    <div class="py-8 sm:py-16">
+        <div class="mx-auto max-w-7xl py-8">
+
+            <h2 class="text-center max-w-6xl mx-auto text-xl font-bold tracking-tight text-white sm:text-3xl md:text-4xl mb-8">
+                {{ $title }}
+            </h2>
+
+            {{ $slot }}
+        </div>
+    </div>
+
+    @svg('n2p-secondary-bottom-divider', 'w-full')
+
+</section>
