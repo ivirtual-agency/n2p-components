3bf217a6f707663405b208d0f0585895e7c01a79
Francisco Kraefft <fkraefft@ivirtual.la>
2023-04-03 19:53:36 -0300

    View with SEO method added to facede.

--- src/Net2phone.php
+++ src/Net2phone.php
@@ -2,6 +2,10 @@
 
 namespace iVirtual\Net2phone;
 
+use Artesaos\SEOTools\Facades\SEOTools;
+use Exception;
+use Illuminate\View\View;
+
 class Net2phone
 {
     /**
@@ -19,5 +23,30 @@ class Net2phone
             'mexico' => ['name' => 'Mexico', 'url' => 'https://net2phone.mx'],
             'peru' => ['name' => 'Peru', 'url' => 'https://net2phone.pe'],
         ];
+    }
+
+    /**
+     * Return a view with the corresponding SEO.
+     */
+    public function viewWithSEO(string $viewName, array $data = []): View
+    {
+        $seoName = 'seo/' . $viewName;
+
+        SEOTools::setTitle($this->checkTranslationSEO($seoName . '.title'));
+        SEOTools::setDescription($this->checkTranslationSEO($seoName . '.description'));
+
+        return view($viewName, $data);
+    }
+
+    /**
+     * Check SEO translation exists.
+     */
+    private function checkTranslationSEO(string $key): string
+    {
+        $trans = trans($key);
+
+        throw_if($trans === $key, new Exception('SEO not set: ' . $key));
+
+        return $trans;
     }
 }
