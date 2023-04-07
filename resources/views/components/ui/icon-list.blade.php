94d47bddbbe6d70238b488228d66512825870a54
Francisco Kraefft <fkraefft@ivirtual.la>
2023-04-07 02:35:14 -0300

    Cusom colors in icon list added.

--- resources/views/components/ui/icon-list.blade.php
+++ resources/views/components/ui/icon-list.blade.php
@@ -1,10 +1,10 @@
-@props(['items', 'icon' => 'entypo-check'])
+@props(['items', 'icon' => 'entypo-check', 'textColor' => 'text-gray-500', 'iconColor' => 'text-primary'])
 
 <ul role="list" class="mt-6 space-y-6">
     @foreach ($items as $item)
         <li class="flex">
-            @svg($icon, 'h-6 w-6 flex-shrink-0 text-primary')
-            <span class="ml-3 text-gray-500">{{ $item }}</span>
+            @svg($icon, 'h-6 w-6 flex-shrink-0 ' . $iconColor)
+            <span class="ml-3 {{ $textColor }}">{{ $item }}</span>
         </li>
     @endforeach
 </ul>
