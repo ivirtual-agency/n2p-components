da900bd386fe12b2a7d3cbaaf1e2cba3c8dffdeb
Francisco Kraefft <fkraefft@ivirtual.la>
2024-09-02 20:57:05 -0300

    Fix in mobile menu.

--- resources/views/components/header/mobile-item.blade.php
+++ resources/views/components/header/mobile-item.blade.php
@@ -1,4 +1,11 @@
-<a href="{{ route($route) }}" class="-m-3 flex items-center rounded-md p-3 hover:bg-gray-50">
+@props(['href', 'route'])
+
+<a {{ $attributes
+    ->except(['href', 'route'])
+    ->merge([
+        'class' => '-m-3 flex items-center rounded-md p-3 hover:bg-gray-50',
+        'href' => $href ?? route($route),
+]) }}>
     <span class="ml-3 text-base font-medium text-gray-900">
         {{ $slot }}
     </span>
